401ed798d3a6d4b91ecfb09558ac25443004c4e7
GaganAcharya2183 <gaganacharya@outlook.com>
2022-01-25 22:22:22 -0800

    Third Commit

--- src/main/java/com/example/myfirstspringapp/MyfirstspringappApplication.java
+++ src/main/java/com/example/myfirstspringapp/MyfirstspringappApplication.java
@@ -1,21 +1,12 @@
 package com.example.myfirstspringapp;
 
-import com.example.myfirstspringapp.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-
 public class MyfirstspringappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstspringappApplication.class, args);
 	}
-
 
 }
